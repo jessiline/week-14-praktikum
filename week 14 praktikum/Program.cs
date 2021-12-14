b14e1804c428cc3dc0f00866a0360d74bb050dba
jessiline <jimanuela@student.ciputra.ac.id>
2021-12-15 00:19:23 +0700

    memperbaiki code sbelumnya, benerin tabel alignment, rapihin code deret prima & bukan

--- week 14 praktikum/Program.cs
+++ week 14 praktikum/Program.cs
@@ -8,52 +8,53 @@ namespace praktikum_w14
     {
         public static void prima(int input, int cek)
         {
-
-            var list = new List<int>();
-            if (cek != 2)
+            int angka = 0;
+            int prima = 0;
+            for(int k = 0; k < input; k++)
             {
-                Console.Write("1 ");
+                for(int q = 0; q< input; q++)
+                {
+                    prima = 0;
+                    while (prima == 0)
+                    {
+                        angka++;
+                        bool cekPrima = true;
+                        if(angka == 1)
+                        {
+                            cekPrima = false;
+                        }
+                        for (int j = 2; j < angka; j++)
+                        {
+                            if ((angka % j) == 0)
+                            {
+                                cekPrima = false;
+                            }
+                        }
+                        if (cekPrima == true && angka != 1 && cek ==2)
+                        {
+                            prima = 1;
+                            Console.Write($"{angka} ".PadLeft(5));
+                        }
+                        if (cekPrima == false && cek !=2)
+                        {
+                            prima = 1;
+                            Console.Write($"{angka} ".PadLeft(5));
+                        }
+                        cekPrima = true;
+                    }
+                }
+                Console.WriteLine();
             }
-            for (int i = 2; i <= 10000; i++)
-            {
-                bool cekPrima = true;
-                for (int j = 2; j < i; j++)
-                {
-                    if ((i % j) == 0)
-                    {
-                        cekPrima = false;
-                    }
-                }
-                if (cekPrima == true && list.Count < input * input && cek == 2)
-                {
-
-                    string t = string.Format("{0,-8}", i);
-                    Console.Write(t);
-                    if (list.Count % input == 0 && i != 2)
-                    {
-                        Console.WriteLine();
-                    }
-                    list.Add(i);
-                }
-                if (cekPrima == false && list.Count < input * input - 1 && cek != 2)
-                {
-                    string y = string.Format("{0,-8}", i);
-                    Console.Write(y);
-                    if (list.Count % input == 0)
-                    {
-                        Console.WriteLine();
-                    }
-                    list.Add(i);
-                }
-
-                cekPrima = true;
-            }
-
         }
         static void Main(string[] args)
         {
             Console.Write("Input : ");
             int input = Convert.ToInt32(Console.ReadLine());
+            while(input < 2)
+            {
+                Console.Write("Input terlalu kecil ! silahkan input ulang : ");
+                input = Convert.ToInt32(Console.ReadLine());
+            }
             int cek = 0;
             for (int i = 1; i <= input; i++)
             {
